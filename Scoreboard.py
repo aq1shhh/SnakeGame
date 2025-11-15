8293520f82e34f6ead22c66d7a246639b09cb9b7
aq1shhh <aishwarysinghsans2@gmail.com>
2025-11-15 07:23:57 +0530

    Initial commit

---
+++ Scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+from snake import Snake
+
+
+class Score(Turtle):
+    def __init__(self):
+
+        super().__init__()
+        self.update = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 220)
+
+        self.hideturtle()
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.write(f"Score: {self.update}", align="center", font=("Verdana", 15, "normal"))
+
+    def increase_score(self):
+        self.update+=1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.color("white")
+        self.goto(0,0)
+        self.write("Game Over", align="center", font=("Arial", 30, "normal"))
+
+
+
---
+++ main.py
@@ -0,0 +1,66 @@
+import turtle
+
+from snake import Snake
+from turtle import Screen
+from Food import Food
+import time
+from Scoreboard import Score
+
+screen=Screen()
+
+screen.setup(height=500, width=500)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake=Snake()
+food=Food()
+score = Score()
+snake.create_snake()
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.w)
+screen.onkey(key="Down", fun=snake.s)
+screen.onkey(key="Left", fun=snake.a)
+screen.onkey(key="Right", fun=snake.d)
+
+
+   
+
+
+
+
+
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+    #detecting food cxolison
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    #detecting collision with the wall
+    if snake.head.xcor()>243 or snake.head.xcor()<-243 or snake.head.ycor()>243 or snake.head.ycor()<-243:
+        game_is_on=False
+        score.game_over()
+
+    #detecting with tail collison
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment)<15:
+            game_is_on=False
+            score.game_over()
+
+
+
+
+screen.exitonclick()
